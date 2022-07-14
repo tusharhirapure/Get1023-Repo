588bb51f669e8bcd2a6edc1e7211d5060c5e4777
Tushar Hirapure <104510922+tusharhirapure@users.noreply.github.com>
2022-07-14 17:20:36 +0530

    Update Sample.java

--- Sample.java
+++ Sample.java
@@ -2,5 +2,6 @@ class Sample{
 public static void main(String args[]){
 System.out.println("Hello Jenkins");
   System.out.println("Hello Jenkins Bhai From Simant");
+   System.out.println("Dekh update hote hi mai badal gaya");
 }
 }
